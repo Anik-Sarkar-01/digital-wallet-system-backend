8d3aa41d0b83f8c772fde7da5c46c9d2c7e31a66
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-08-31 01:56:18 +0600

    send response util added to send consistent response message

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,23 +3,28 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 
-const createUser = catchAsync(async(req: Request, res: Response, next: NextFunction) => {
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
-    res.status(httpStatus.CREATED).json({
+
+    sendResponse(res, {
         success: true,
+        statusCode: httpStatus.CREATED,
         message: "User created successfully",
-        user,
+        data: user,
     })
 })
 
-const getAllUsers = catchAsync(async(req: Request, res: Response, next: NextFunction) => {
-    const users = await UserServices.getAllUsers();
-    res.status(httpStatus.OK).json({
+const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const result = await UserServices.getAllUsers();
+    sendResponse(res, {
         success: true,
+        statusCode: httpStatus.OK,
         message: "Users retrieved successfully",
-        users,
+        data: result.data,
+        meta: result.meta,
     })
 })
 
