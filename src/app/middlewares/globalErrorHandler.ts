8a2bc85e5d7e9c789b95e1398ca8f570cc313281
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-08-30 22:22:08 +0600

    custom error class added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,11 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
+import AppError from "../errorHelpers/AppError";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    res.status(500).json({
+
+    let statusCode = 500
+    let message = `Something went wrong! ${err.message}`
+
+    if(err instanceof AppError) {
+        statusCode = err.statusCode
+        message = err.message
+    }
+
+    res.status(statusCode).json({
         success: false,
-        message: `Something went wrong! ${err.message}`,
+        message,
+        err,
         stack: envVars.NODE_ENV === "development" ? err.stack : null
     });
 }
